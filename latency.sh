6704e71ad6ac8ea31777c14fdcbcc9175b428b3d
Stephen Bates <stephen.bates@pmcs.com>
2015-04-09 18:18:00 -0700

    Changed DEVICE to FILENAME
    Makes naming consistent with FIO. Prepare for being able to do both
    block devices and files on block devices.

--- latency.sh
+++ latency.sh
@@ -29,23 +29,23 @@ SKIP=10000
 CROP=1000
 
   # Parameters for running FIO
-DEVICE=/dev/nvme0n1
+FILENAME=/dev/nvme0n1
 NUM_JOBS=1
 SIZE=1G
 IO_DEPTH=1
 BLOCK_SIZE=512
 COUNT=$((100000 + ${SKIP} + ${CROP}))
-LAT_LOG=$(basename ${DEVICE})
+LAT_LOG=$(basename ${FILENAME})
 RW_MIX_READ=100
 
   # Accept some key parameter changes from the command line.
-while getopts "r:n:d:i:" opt; do
+while getopts "r:n:f:i:" opt; do
     case "$opt" in
 	r)  RW_MIX_READ=${OPTARG}
             ;;
 	n)  NUM_JOBS=${OPTARG}
             ;;
-	d)  DEVICE=${OPTARG}
+	f)  FILENAME=${OPTARG}
             ;;
 	i)  IO_DEPTH=${OPTARG}
             ;;
@@ -65,13 +65,13 @@ function cleanup {
     mv ${LAT_LOG}_lat.1.log ${LAT_LOG}.log
 }
 
-if [ ! -b "$DEVICE" ]; then
+if [ ! -b "$FILENAME" ]; then
      echo "latency.sh: You must specify a block IO device"
      exit 1
 fi
 
 rm *.log
-DEVICE=${DEVICE} SIZE=${SIZE} NUM_JOBS=${NUM_JOBS} IO_DEPTH=${IO_DEPTH} \
+FILENAME=${FILENAME} SIZE=${SIZE} NUM_JOBS=${NUM_JOBS} IO_DEPTH=${IO_DEPTH} \
     BLOCK_SIZE=${BLOCK_SIZE} COUNT=${COUNT} RW_MIX_READ=${RW_MIX_READ} \
     LAT_LOG=${LAT_LOG} fio ./fio-scripts/latency.fio
 cleanup
