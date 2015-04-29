0c3f078f70e045c34a541b87955657548824a553
Stephen Bates <sbates@raithlin.com>
2015-04-28 19:22:05 -0600

    Added CPU measurement tool in tools folder

---
+++ tools/cpuperf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+import subprocess as sp
+import time
+
+def get_perfdata(options):
+    try:
+        data = sp.check_output(["ps", "-C", options.command, "-o" "%cpu=,%mem="])
+        data = tuple(float(x) for x in data.split())
+    except sp.CalledProcessError:
+        if (options.skip):
+            data=None
+        else:
+            data = 0.,0.
+
+    return data
+
+
+if __name__=="__main__":
+    import optparse
+
+    parser = optparse.OptionParser()
+    parser.add_option("-C", "--command", action="store", type="string",
+                      help="The command to look for in the ps log.", default="textswap")
+    parser.add_option("-t", "--time", action="store", type="int",
+                      help="Time to run for in seconds (-1 to run forever)", default=-1)
+    parser.add_option("-w", "--wait", action="store", type="int",
+                      help="Wait time in ms between calls to ps.", default=100)
+    parser.add_option("-s", "--skip", action="store_true",
+                      help="Only output data when command is running.")
+    (options, args) = parser.parse_args()
+
+    try:
+        start_time = time.time()
+        end_time = start_time + options.time
+        print("#%7s   %3s   %3s" % ("TIME", "CPU", "MEM"))
+        while options.time < 0 or time.time() < end_time:
+            t = time.time()-start_time
+            data = get_perfdata(options)
+            if data:
+                print("%8.1f   %-3.1f   %3.1f" % ((t,) + data))
+                sys.stdout.flush()
+            time.sleep(options.wait / 1000.)
+
+    except KeyboardInterrupt:
+        print
