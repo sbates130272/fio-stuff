e2ce116fe17e3d7450182f0113eefc6a04791791
Stephen Bates <sbates@raithlin.com>
2023-11-15 15:34:59 -0700

    improve-ci: Make some improvements to the CI script.
    We make some changes to run_suite.sh and the GitHub smoke-test. This
    means the CI takes a reasonable length of time and there is a sanity
    check on the number of non-zero PNG files generated.
    
    Fixes #11.

--- run_suite.sh
+++ run_suite.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+GITHUB_CI=${GITHUB_CI:-"false"}
+PNG_EXPECT=${PNG_EXPECT:-14}
 DIR=$(realpath $(dirname "$0"))
 
 set -e
+
+if [ "${GITHUB_CI}" == "true" ]; then
+    pjobs=(1)
+    pbs=(512)
+else
+    pjobs=(1 2 4 8 16 32 64)
+    pbs=(512 4k 8k 16k 64k 128k 1M)
+fi
 
 OUTDIR=bs_job
 mkdir -p $OUTDIR
@@ -16,10 +26,21 @@ OUTDIR=threads_job
 mkdir -p $OUTDIR
 $DIR/threads.sh "$@" -o $OUTDIR
 
-for jobs in 1 2 4 8 16 32 64; do
-    for bs in 512 4k 8k 16k 64k 128k 1M; do
-	OUTDIR=iodepth_jobs_${jobs}_bs_${bs}
+for jobs in $pjobs; do
+    for bs in $pbs; do
+	OUTDIR=iodepth_job_${jobs}_bs_${bs}
 	mkdir -p $OUTDIR
 	$DIR/iodepth.sh "$@" -b $bs -n $jobs -o $OUTDIR
     done
 done
+
+  # During CI testing ensure the appropriate number of non-zero sized
+  # PNG files have been created.
+
+if [ "${GITHUB_CI}" == "true" ]; then
+    PNG_COUNT=$(find . -name *.png -size +0 | wc -l)
+    if [ ${PNG_COUNT} -ne ${PNG_EXPECT} ]; then
+	echo "Error in expected number of PNG files (${PNG_COUNT}!=${PNG_EXPECT})"
+	exit 1
+    fi
+fi
