d31b19d5f5c8ca9d36ee1aa3d40407afe7e36c91
Stephen Bates <sbates@raithlin.com>
2023-11-15 14:23:44 -0700

    run_suite.sh: Move the scripts into their own subfolders.
    Make this a cleaner script by making all the tests run in a
    subfolder. This makes post-run cleanup a lot tidier. Also fix a
    misplaced echo so the script actually runs as expected.

--- run_suite.sh
+++ run_suite.sh
@@ -4,14 +4,22 @@ DIR=$(realpath $(dirname "$0"))
 
 set -e
 
-$DIR/bs.sh "$@" -o .
-$DIR/latency.sh "$@" -o .
-$DIR/threads.sh "$@" -o .
+OUTDIR=bs_job
+mkdir -p $OUTDIR
+$DIR/bs.sh "$@" -o $OUTDIR
+
+OUTDIR=latency_job
+mkdir -p $OUTDIR
+$DIR/latency.sh "$@" -o $OUTDIR
+
+OUTDIR=threads_job
+mkdir -p $OUTDIR
+$DIR/threads.sh "$@" -o $OUTDIR
 
 for jobs in 1 2 4 8 16 32 64; do
     for bs in 512 4k 8k 16k 64k 128k 1M; do
 	OUTDIR=iodepth_jobs_${jobs}_bs_${bs}
 	mkdir -p $OUTDIR
-	echo $DIR/iodepth.sh "$@" -b $bs -n $jobs -o $OUTDIR
+	$DIR/iodepth.sh "$@" -b $bs -n $jobs -o $OUTDIR
     done
 done
